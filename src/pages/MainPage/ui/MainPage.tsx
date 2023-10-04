d280a30375f7beef192a3f2840c3a0239dea37c0
SuleymYovloy <suleymevlo81@gmail.com>
2023-10-04 12:58:32 +0300

    add babel, add extract plugin, fix bag

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,12 +4,7 @@ import { useTranslation } from "react-i18next";
 const MainPage = () => {
   const { t } = useTranslation("main");
 
-  return (
-    <div>
-      {t("Главная")}
-      {t("Главная")}
-    </div>
-  );
+  return <div>{t("Главная")}</div>;
 };
 
 export default MainPage;
