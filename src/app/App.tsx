43f94192f8ddef146fbacb206897505be96ff17a
Suleym <suleymevlo81@gmail.com>
2023-10-06 17:32:35 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,24 @@
-import React, { Suspense } from 'react';
-import './styles/index.scss';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { useTheme } from './providers/ThemeProvider';
+import React, { Suspense } from "react";
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { useTheme } from "./providers/ThemeProvider";
 
-import { AppRouter } from './providers/router';
+import { AppRouter } from "./providers/router";
 
 export const App = () => {
-    const { theme } = useTheme();
+	const { theme } = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                sdfsdf
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-        </div>
-    );
+	return (
+		<div className={classNames("app", {}, [theme])}>
+			<Suspense fallback="">
+				<Navbar />
+				<div className="content-page">
+					<Sidebar />
+					<AppRouter />
+				</div>
+			</Suspense>
+		</div>
+	);
 };
