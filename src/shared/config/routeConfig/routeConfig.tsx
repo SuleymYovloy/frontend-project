af7a311df2e9d6c399ccaf8943ea35cb9a585c97
SuleymYovloy <suleymevlo81@gmail.com>
2023-10-09 13:15:29 +0300

    use PadeLoader, add css-loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { NotFoundPage } from "pages/NotFoundPage";
 import { RouteProps } from "react-router-dom";
 
 export enum AppRoutes {
   MAIN = "main",
   ABOUT = "about",
+	NOT_FOUND = "not_found"
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
 	[AppRoutes.MAIN]: "/",
 	[AppRoutes.ABOUT]: "/about",
+	[AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -18,7 +21,11 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
 		element: <MainPage />,
 	},
 	[AppRoutes.ABOUT]: {
-		path: RoutePath.main,
+		path: RoutePath.about,
 		element: <AboutPage />,
+	},
+	[AppRoutes.NOT_FOUND]: {
+		path: RoutePath.not_found,
+		element: <NotFoundPage />,
 	},
 };
