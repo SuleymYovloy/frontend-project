581bff47f3b6c88bf527efc42518cacdb7f90677
Suleym <suleymevlo81@gmail.com>
2023-11-04 00:44:00 +0300

    add storybook finish

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -4,30 +4,34 @@ import { Link, LinkProps } from "react-router-dom";
 import cls from "./AppLink.module.scss";
 
 export enum AppLinkTheme {
-  PRIMARY = "primary",
-  SECONDARY = "secondary",
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
+    RED = "red",
 }
 
 interface AppLinkProps extends LinkProps {
-  className?: string;
-  theme?: AppLinkTheme;
+    className?: string;
+    theme?: AppLinkTheme;
 }
 
 export const AppLink: FC<AppLinkProps> = (props) => {
-	const {
-		to,
-		children,
-		className,
-		theme = AppLinkTheme.PRIMARY,
-		...otherProps
-	} = props;
-	return (
-		<Link
-			to={to}
-			className={classNames(cls.AppLink, { theme }, [className, cls[theme]])}
-			{...otherProps}
-		>
-			{children}
-		</Link>
-	);
+    const {
+        to,
+        children,
+        className,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props;
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, { theme }, [
+                className,
+                cls[theme],
+            ])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
 };
