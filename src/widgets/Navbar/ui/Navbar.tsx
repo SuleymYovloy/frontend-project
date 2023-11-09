61f43bf35669fe315a3e2f2c036ae19072f1e3b7
Suleym <suleymevlo81@gmail.com>
2023-11-09 16:31:30 +0300

    fix lint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -10,13 +10,11 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => (
   <div className={classNames(cls.Navbar, {}, [className])}>
     <div className={cls.links}>
-      <AppLink
-        theme={AppLinkTheme.SECONDARY}
-        to="/"
-        className={cls.mainLink}
-      >
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
         Главная
       </AppLink>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
         О сайте
       </AppLink>
