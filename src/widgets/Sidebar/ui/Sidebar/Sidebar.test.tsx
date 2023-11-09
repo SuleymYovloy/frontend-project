31067e713a7ce43c8780bd25f92a05846c20b5e9
Suleym <suleymevlo81@gmail.com>
2023-11-09 16:36:21 +0300

    fix lint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,17 @@
-import '@testing-library/jest-dom';
 import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
-  test('with only first param', () => {
+  test('should be rendered', () => {
     renderWithTranslation(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
-  test('test toggle', () => {
+
+  test('test toggle button', () => {
     renderWithTranslation(<Sidebar />);
-    const toggleBtn = screen.getByTestId('sidebar-toggle');
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
-    fireEvent.click(toggleBtn);
+    const toggleButton = screen.getByTestId('sidebar-toggle');
+    fireEvent.click(toggleButton);
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
   });
 });
