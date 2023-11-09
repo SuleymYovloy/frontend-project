fbb3fda2774a5af01e04d069bbd65ccb03d5122a
Suleym <suleymevlo81@gmail.com>
2023-11-09 15:27:13 +0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,62 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"jest": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended"
-	],
-	"overrides": [
-		{
-			"env": {
-				"node": true
-			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint",
-		"react"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows",
-			0
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
+  rules: {
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    indent: [2, 2],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.tsx'],
+    }],
+    'linebreak-style': 0,
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/no-extraneous-dependencies': 'warn',
+    'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      onlyAttribute: ['t', 'i18nKey'],
+      ignoreAttribute: ['data-testid', 'to'],
+    }],
+    'max-len': ['error', {
+      code: 120,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react/button-has-type': 'off', // TODO: check this rule
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'no-param-reassign': 'off',
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+  overrides: [{
+    files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off',
+      'max-len': 'off',
+    },
+  }],
 };
