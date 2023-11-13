0e7e007927c3e4528e0190963ad561e18f32f832
SuleymYovloy <suleymevlo81@gmail.com>
2023-11-13 15:39:37 +0300

    add Modal, add Portal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,27 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { useTheme } from './providers/ThemeProvider';
 
 import { AppRouter } from './providers/router';
 
 export const App = () => {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorum esse fuga fugiat laudantium modi nisi non numquam, pariatur quae saepe soluta suscipit tempora unde, vel vitae voluptatem!
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
