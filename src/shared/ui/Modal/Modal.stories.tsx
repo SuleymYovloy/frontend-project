c1eb0444654412f58a08934c74d51c73450c0676
Suleym <suleymevlo81@gmail.com>
2023-11-13 22:27:09 +0300

    fix modal stories

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
+import { Modal } from 'shared/ui/Modal/Modal';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-// import * as trace_events from 'trace_events';
-import { Modal } from './Modal';
 
 export default {
     title: 'shared/Modal',
@@ -19,12 +17,12 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorum esse fuga fugiat laudantium modi nisi non numquam, pariatur quae saepe soluta suscipit tempora unde, vel vitae voluptatem!\n',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
+
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorum esse fuga fugiat laudantium modi nisi non numquam, pariatur quae saepe soluta suscipit tempora unde, vel vitae voluptatem!\n',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
-
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
