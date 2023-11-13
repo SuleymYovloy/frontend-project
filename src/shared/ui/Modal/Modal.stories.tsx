0653ebc771adaf637b032c232a9c11f53f5a33de
Suleym <suleymevlo81@gmail.com>
2023-11-13 22:54:10 +0300

    fix modal stories

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -26,3 +26,4 @@ Dark.args = {
     children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+// fix
