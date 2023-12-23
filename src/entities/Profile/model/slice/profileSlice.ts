f86462dafbca1362e8e610d5495fa64c39a5fdae
Suleym <suleymevlo81@gmail.com>
2023-12-23 17:58:00 +0300

    use article, add skeleton

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -64,19 +64,3 @@ export const profileSlice = createSlice({
 
 export const { actions: profileActions } = profileSlice;
 export const { reducer: profileReducer } = profileSlice;
-
-//  "loki": {
-//    "configurations": {
-//      "chrome.laptop": {
-//        "target": "chrome.app",
-//        "width": 1366,
-//        "height": 768,
-//        "deviceScaleFactor": 1,
-//        "mobile": false
-//      },
-//      "chrome.iphone7": {
-//        "target": "chrome.app",
-//        "preset": "iPhone 7"
-//      }
-//    }
-//  }
