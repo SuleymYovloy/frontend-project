1418f4c6676b3461c0c573517571265b080deec5
Suleym <suleymevlo81@gmail.com>
2024-03-10 10:27:07 +0300

    add helpers for slice, selectors, actions

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { RatingCard } from '@/entities/Rating';
+import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -13,11 +13,8 @@ const MainPage = () => {
 
     return (
         <Page>
+            <Counter />
             {t('Главная страница')}
-            <RatingCard
-                title="Как вам статья"
-                feedbackTitle="Оставьте отзыв о статье"
-            />
         </Page>
     );
 };
