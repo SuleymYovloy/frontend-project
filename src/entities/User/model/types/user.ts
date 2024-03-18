9e6d00b60dfa4429344d11dddee057b109accf16
Suleym <suleymevlo81@gmail.com>
2024-03-18 23:38:20 +0300

    add feature flags concept, articlesPage styles fix

--- src/entities/User/model/types/user.ts
+++ src/entities/User/model/types/user.ts
@@ -1,13 +1,16 @@
 import { UserRole } from '../consts/consts';
+import { FeatureFlags } from '@/shared/types/featureFlags';
 
 export interface User {
     id: string;
     username: string;
     avatar?: string;
     roles?: UserRole[];
+    features?: FeatureFlags;
 }
 
 export interface UserSchema {
     authData?: User;
+
     _inited: boolean;
 }
