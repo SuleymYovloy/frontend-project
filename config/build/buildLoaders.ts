f9152dfcdd350b4c77c6d3c2c0d5a6f76a7f0b67
Suleym <suleymevlo81@gmail.com>
2024-03-24 18:01:38 +0300

    fix icons size and color

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,18 +1,33 @@
 import webpack from 'webpack';
-import { BuildOptions } from './types/config';
-import { buildCssLoader } from './loaders/buildCssLoader';
-import { buildBabelLoader } from './loaders/buildBabelLoader';
+import {BuildOptions} from './types/config';
+import {buildCssLoader} from './loaders/buildCssLoader';
+import {buildBabelLoader} from './loaders/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    const { isDev } = options;
+    const {isDev} = options;
 
     const svgLoader = {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: [{
+            loader: '@svgr/webpack',
+            options: {
+                icon: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'convertColors',
+                            params: {
+                                currentColor: true
+                            }
+                        }
+                    ]
+                }
+            }
+        }],
     };
 
-    const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
-    const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
+    const codeBabelLoader = buildBabelLoader({...options, isTsx: false});
+    const tsxCodeBabelLoader = buildBabelLoader({...options, isTsx: true});
 
     const cssLoader = buildCssLoader(isDev);
 
